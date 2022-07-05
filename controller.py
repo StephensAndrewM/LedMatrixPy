92fb91ed619a2e819c5032e1e2ef7ebb87388697
Andrew Stephens <andrewmediaprod@gmail.com>
2022-07-05 17:59:52 -0400

    Fix type errors in controller

--- controller.py
+++ controller.py
@@ -1,31 +1,33 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
 from slideshow import Slideshow
 
 
 class ControllerRequestHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
+
+    def do_POST(self) -> None:
         if self.path == "/start":
             logging.info("Starting slideshow")
-            self.server.slideshow.start()
+            self.server.slideshow.start()  # type: ignore
         elif self.path == "/stop":
             logging.info("Stopping slideshow")
-            self.server.slideshow.stop()
+            self.server.slideshow.stop()  # type: ignore
         elif self.path == "/freeze":
             logging.info("Freezing slideshow")
-            self.server.slideshow.freeze()
+            self.server.slideshow.freeze()  # type: ignore
         elif self.path == "/unfreeze":
             logging.info("Unfreezing slideshow")
-            self.server.slideshow.unfreeze()
+            self.server.slideshow.unfreeze()  # type: ignore
         elif self.path == "/shutdown":
             logging.info("Shutting down slideshow")
-            self.server.slideshow.stop()
-            self.server.stopped = True
+            self.server.slideshow.stop()  # type: ignore
+            self.server.stopped = True  # type: ignore
         else:
             logging.warning("Unknown controller endpoint %s", self.path)
             self.send_response(200)
 
-    def do_GET(self):
+    def do_GET(self) -> None:
         self.send_response(405)
 
 
@@ -33,12 +35,12 @@ class ControllerServer(HTTPServer):
     slideshow: Slideshow
     stopped: bool
 
-    def __init__(self, slideshow):
+    def __init__(self, slideshow: Slideshow) -> None:
         super(("localhost", 5000), ControllerRequestHandler)
         self.slideshow = slideshow
         self.stopped = False
 
-    def serve_forever(self):
+    def serve_forever(self, poll_interval: float = 0.5) -> None:
         while not self.stopped:
             self.handle_request()
 
@@ -46,7 +48,7 @@ class ControllerServer(HTTPServer):
 class Controller:
     server: ControllerServer
 
-    def __init__(self, slideshow) -> None:
+    def __init__(self, slideshow: Slideshow) -> None:
         self.server = ControllerServer(slideshow)
 
     def run_until_shutdown(self) -> None:
